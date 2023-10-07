ebf78308bd13548d1068bbec572673305e7fd4a8
Amir Hamzah <46454997+HellRyder43@users.noreply.github.com>
2023-10-08 02:22:25 +0800

    Added datatables. Added fontawesome. Added sweet alert. Added automapper.

---
+++ LeaveManagement/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 100, ErrorMessage = "Please enter a valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
